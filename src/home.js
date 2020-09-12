1876786a9e08d0be3868d332b76ba688038ff9e3
Filip Antala <fifcostyle@gmail.com>
2020-09-12 13:37:35 +0200

    initial commit

---
+++ src/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './home.css'
+
+export default function Home() {
+    return (
+        <h1 class="header">Vitajte na stránke našeho muzikálu!</h1>
+    )
+}
---
+++ src/registration.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './registration.css'
+
+export default function Registration() {
+    return (
+        <p>REGISTRATION</p>
+    )
+}
