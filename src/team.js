3eef955e8c07cf2ffb18693f2e4790374f777fc1
Filip Antala <fifcostyle@gmail.com>
2020-09-14 15:09:42 +0200

    added Team page

--- src/team.js
+++ src/team.js
@@ -8,45 +8,45 @@ export default function Team() {
                 <h1>Náš Soví team</h1>
             </div>
             <div>
-            <div class="division herci">
-                <h2>Herci</h2> 
-                <p>Adela Zemaníková ako <em>Ashley Cooperová</em></p>
-                <p>Leo Rehúš ako <em>postava</em></p>
-                <p>Libor Byrtus ako <em>postava</em></p>
-                <p>Martin Krchnavý ako <em>postava</em></p>
-                <p>Michal Földeš ako <em>postava</em></p>
-                <p>Matúš Košťál ako <em>postava</em></p>
-            </div>
-            <div class="division scenaristi">
-                <h2>Scenáristi</h2>
-                <p>Terezka Kršáková</p>
-                <p>Matilda Borovská</p>
-                <p>Vesna Vrábelová</p>
-                <p>Zara Halmo</p>
-                <p>Šimon Godarský</p>
-            </div>
-            <div class="division manazeri">
-                <h2>Manažéri</h2>
-                <p>Filip Antala</p>
-                <p>Daniel Držík</p>
-                <p>Matúš Halák</p>
-                <p>Michal Svoboda</p>
-            </div>
-            <div class="division kulisari">
-                <h2>Kulisári</h2>
-                <p>Klára Šumská</p>
-                <p>Sofia Kocková</p>
-            </div>
-            <div class="division kostymeri">
-                <h2>Kostyméri</h2>
-                <p>Adela Belovičová</p>
-                <p>Zoja Zajvaldová</p>
-            </div>
-            <div class="division hudobnici">
-                <h2>Hudobníci</h2>
-                <p>Martin Krchnavý</p>
-                <p>Matúš Košťál</p>
-            </div>
+                <div class="division herci">
+                    <h2>Herci</h2> 
+                    <p>Adela Zemaníková ako <em>Ashley Cooperová</em></p>
+                    <p>Leo Rehúš ako <em>postava</em></p>
+                    <p>Libor Byrtus ako <em>postava</em></p>
+                    <p>Martin Krchnavý ako <em>postava</em></p>
+                    <p>Michal Földeš ako <em>postava</em></p>
+                    <p>Matúš Košťál ako <em>postava</em></p>
+                </div>
+                <div class="division scenaristi">
+                    <h2>Scenáristi</h2>
+                    <p>Terezka Kršáková</p>
+                    <p>Matilda Borovská</p>
+                    <p>Vesna Vrábelová</p>
+                    <p>Zara Halmo</p>
+                    <p>Šimon Godarský</p>
+                </div>
+                <div class="division manazeri">
+                    <h2>Manažéri</h2>
+                    <p>Filip Antala</p>
+                    <p>Daniel Držík</p>
+                    <p>Matúš Halák</p>
+                    <p>Michal Svoboda</p>
+                </div>
+                <div class="division kulisari">
+                    <h2>Kulisári</h2>
+                    <p>Klára Šumská</p>
+                    <p>Sofia Kocková</p>
+                </div>
+                <div class="division kostymeri">
+                    <h2>Kostyméri</h2>
+                    <p>Adela Belovičová</p>
+                    <p>Zoja Zajvaldová</p>
+                </div>
+                <div class="division hudobnici">
+                    <h2>Hudobníci</h2>
+                    <p>Martin Krchnavý</p>
+                    <p>Matúš Košťál</p>
+                </div>
             </div>
         </div>
     )
