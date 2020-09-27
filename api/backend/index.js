c2e64731ba023534ed521c8c10e0987b396ca090
Filip Antala <fifcostyle@gmail.com>
2020-09-27 18:23:48 +0200

    backend changes

---
+++ api/backend/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+const con = mysql.createConnection({
+    host: "countrycraft-db.cvitton2dtqy.eu-west-3.rds.amazonaws.com",
+    user: "muzikal",
+    password: "muzikal123",
+    database: "tickets"
+})
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.websupport.sk",
+    port: 465,
+    secure: true,
+    auth: {
+        user: "automailer@muzikalvrazdapodlaobete.sk",
+        pass: "Jr75}etp$(",
+    },
+});
+
+app.post('/registerticket', (req, res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var qty = req.body.qty;
+    var origin = req.body.origin;
+
+    var sql = `INSERT INTO tickets (name, email, qty, origin, state) VALUES ('${name}', '${email}', '${qty}', '${origin}', 'RESERVED')`;
+    
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Inserted!")
+
+        transporter.sendMail({
+            from: 'automailer@muzikalvrazdapodlaobete.sk',
+            replyTo: "rezervacie@muzikalvrazdapodlaobete.sk",
+            to: req.body.email,
+            bcc: "rezervacie@muzikalvrazdapodlaobete.sk",
+            subject: "Rezervácia č. " + result.insertId,
+            text: "test",
+            html: '<p>test</p>',
+        });
+
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
