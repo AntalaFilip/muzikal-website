58cd70720a0aa24f9f47fd23bdb68d3072d9e34c
FifcoStyle <fifcostyle@gmail.com>
2020-10-09 17:24:50 +0200

    added tooltips

--- src/team.js
+++ src/team.js
@@ -3,19 +3,6 @@ import './team.css'
 
 
 class Team extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            toolTip: false,
-            ttId: null
-        }
-        this.changetooltip.bind(this.changetooltip);
-    }
-    changetooltip(event) {
-        this.setState({
-            toolTip: true
-        })
-    }
     render() {
         return (
             <div className="team">
@@ -24,21 +11,45 @@ class Team extends React.Component {
                 </div>
                 <div>
                     <div className="division herci">
-                        {this.state.toolTip && 
-                        <div className="tooltip">
-                            {this.ttId === `acooper` && 
-                            <p>Ashley Cooperová, snúbenica Steva Greena</p> }
-                        </div> 
-                        }
                         <h2>Herci</h2>
-                        <p>Adela Zemaníková ako <em id="acooper" onMouseOver={this.changetooltip}>Ashley Cooperová</em></p>
-                        <p>Leo Rehúš ako <em id="bjones">Detektív Brandon Jones</em></p>
-                        <p>Michal Čmelík ako <em id="sgreen">Steve Green</em></p>
-                        <p>Samo Pekarčík ako <em id="dgreen">Dustin Green</em></p>
-                        <p>Libor Byrtus ako <em id="dnorton">Donte Norton</em></p>
-                        <p>Michal Földeš ako <em id="everner">Ethan Verner</em></p>
-                        <p>Martin Krchnavý ako <em id="doctor">Doktor</em></p>
-                        <p>Matúš Košťál ako <em id="servant">Sluha</em></p>
+                        <p>Adela Zemaníková ako&nbsp;
+                            <em id="acooper" className="tooltip">Ashley Cooperová
+                                <span className="tooltiptxt">Ashley Cooperová, snúbenica Steva Greena</span>
+                            </em>
+                        </p>
+                        <p>Leo Rehúš ako&nbsp;
+                            <em id="bjones" className="tooltip">Detektív Brandon Jones
+                                <span className="tooltiptxt">Brandon Jones, súkromný detektív</span>
+                            </em>
+                        </p>
+                        <p>Michal Čmelík ako&nbsp;
+                            <em id="sgreen" className="tooltip">Steve Green
+                                <span className="tooltiptxt">Steve Green, úspešný spisovateľ detektívok, snúbenec Ashley Cooperovej</span>
+                            </em>
+                        </p>
+                        <p>Samo Pekarčík ako&nbsp;
+                            <em id="dgreen" className="tooltip">Dustin Green
+                                <span className="tooltiptxt">Dustin Green, brat zosnulého Steva Greena. Nikdy si neboli veľmi blízki, ale teraz jeho smrť veľmi oplakáva</span>
+                            </em>
+                        </p>
+                        <p>Libor Byrtus ako&nbsp;
+                            <em id="dnorton" className="tooltip">Donte Norton
+                                <span className="tooltiptxt">Donte Norton, francúzsky cudzinec, ktorý žije v meste už pár rokov, ale nikto si s ním nie je blízky</span>
+                            </em>
+                        </p>
+                        <p>Michal Földeš ako&nbsp;
+                            <em id="everner" className="tooltip">Ethan Verner
+                                <span className="tooltiptxt">Ethan Verner, Stevov najväčší súper vo svete literatúry, pretekali spolu, koho knihy budú úspešnejšie</span>
+                            </em>
+                        </p>
+                        <p>Matúš Košťál ako&nbsp;
+                            <em id="servant" className="tooltip">Peter Granger
+                                <span className="tooltiptxt">Peter Granger, sluha v&nbsp;dome Ashley a Steva</span>
+                            </em>
+                        </p>
+                        <p>Martin Krchnavý ako&nbsp;
+                            <em id="doctor">Doktor</em>
+                        </p>
                     </div>
                     <div className="division scenaristi">
                         <h2>Scenáristi</h2>
