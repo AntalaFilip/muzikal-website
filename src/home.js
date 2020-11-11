d8cfcac4f7c3a6ee684a3601ebcacfba96c3dc88
AntalaFilip <fifcostyle@gmail.com>
2020-11-11 10:54:27 +0100

    added info

--- src/home.js
+++ src/home.js
@@ -3,6 +3,11 @@ import './home.css'
 
 export default function Home() {
     return (
-        <h2 className="home-header">Vitajte na stránke muzikálu Vražda podľa obete!</h2>
+        <div className="home">
+            <h2 className="header">Vitajte na stránke muzikálu Vražda podľa obete!</h2>
+            <p>Táto stránka je v procese vyrábania, ak niečo ešte neviete nájsť, je možné, že za pár dní to pribudne :D</p>
+            <p>Termín muzikálu by sme chceli dať niekedy vo februári, ale bude to dosť závisieť od momentálnej situácie</p>
+            <p>Ak máte podnet na zlepšenie stránky, neváhajte nám napísať na <a href="webmaster@sovy.felixmuzikal.sk">webmaster@sovy.felixmuzikal.sk</a></p>
+        </div>
     )
 }
